77d1b3a40c8c6b8901b60c9b0e05f08b8b3da698
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-02-26 22:54:23 +0100

    removed unused imports

--- app/src/main/java/com/koki/codeivate/ContentLoader.java
+++ app/src/main/java/com/koki/codeivate/ContentLoader.java
@@ -1,6 +1,5 @@
 package com.koki.codeivate;
 
-import android.app.DownloadManager;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.util.Log;
@@ -12,21 +11,11 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 
 /**
